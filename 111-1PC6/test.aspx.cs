77c9f91e876efe9813a7e4f30e9c22213da05938
109111136 <h226810797@gmail.com>
2022-11-13 15:51:48 +0800

    hi

---
+++ 111-1PC6/test.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1PC6
+{
+    public partial class test : System.Web.UI.Page
+    {
+        string[] school = new string[3] { "台科", "北科", "亞東" };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+                for (int i = 0; i < school.Length; i++)
+                {
+                    ListItem C = new ListItem();
+                    C.Text = school[i];
+                    ddl_Test.Items.Add(C);
+
+                }
+        }
+    }
+}
